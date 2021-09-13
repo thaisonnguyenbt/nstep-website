817a9c029db306553982fa24e07a55a38c2b0dba
Thai Son Nguyen <78575430+thaisonnguyendiscovery@users.noreply.github.com>
2021-09-13 20:35:55 +0700

    cleanup

--- web/client-config.js
+++ web/client-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   sanity: {
-    projectId:
-      process.env.GATSBY_SANITY_PROJECT_ID || "8p3kcvtl",
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || "8p3kcvtl",
     dataset: process.env.GATSBY_SANITY_DATASET || "production",
   },
 };
